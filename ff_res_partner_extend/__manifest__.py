5a3c39620a5d597fa6b2dfd58e1877a5e5f338b1
Vyshnav9062 <vyshnav9062@gmail.com>
2025-06-16 14:11:59 +0530

    [ADD]:ff_res_partner_extend

---
+++ ff_res_partner_extend/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Displays Customer Name Only',
+    'version': '17.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Displays Customer Name Only',
+    'depends': ['base','sale'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
